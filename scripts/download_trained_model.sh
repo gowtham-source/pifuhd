43004464637ff5e0f4a501b7489a62c03ccb5b61
Gowtham <75899822+gowtham-source@users.noreply.github.com>
2022-11-29 17:50:41 +0530

    Update download_trained_model.sh

--- scripts/download_trained_model.sh
+++ scripts/download_trained_model.sh
@@ -4,5 +4,5 @@ set -ex
 
 mkdir -p checkpoints
 cd checkpoints
-wget "https://dl.fbaipublicfiles.com/pifuhd/checkpoints/pifuhd.pt" pifuhd.pt
+wget "https://drive.google.com/uc?export=download&id=106QzE8IbysL6abnh-sE5WehSmKU47UO-&confirm=t&uuid=2548fc11-a838-4d3c-af5e-561060a76817&at=ALAFpqzIXX5zCqxQI6_SAmkZ6gdx:1667578816979" pifuhd.pt
 cd ..
